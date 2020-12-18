a24c1027ddc17bc65197a437e7144c9f5a321965
whj <emailwen@qq.com>
2020-12-18 14:38:20 +0800

    fix: 修复代码提示问题, 增加TableHeaderItem类型导出

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,4 @@
 export * from './element-nice-ui'
 import * as ElementUI from './element-nice-ui'
+export * from './lib'
 export default ElementUI
--- types/table.d.ts
+++ types/table.d.ts
@@ -37,43 +37,6 @@ export interface treeNode {
   loaded: boolean
 }
 
-export interface TableColumnItem{
-  /** 列类型 */
-  type?: 'selection' | 'index' | 'expand';
-  /** 自定义索引 */
-  index?: number | ((index: number) => number)
-  /** 列的key值, 如果需要使用 filter-change 事件，则需要此属性标识是哪个 column 的筛选条件 */
-  columnKey?: string;
-  /** 表头的标题 */
-  label?: string;
-  /** 对应列内容的字段名 */
-  prop?: string;
-  /** 对应列的宽度 */
-  width?: string | number;
-  /** 列的最小宽度，与width的区别是 width 是固定的，minWidth 会把剩余宽度按比例分配给设置了 minWidth 的列 */
-  minWidth?: string | number;
-  /** 列是否固定在左侧或者右侧，true 表示固定在左侧 */
-  fixed?: true | 'left' | 'right';
-  /** 对应列是否可以排序, 这是远程排序 */
-  sortable?: boolean;
-  /** 列是否可以拖拽来改变宽度  */
-  resizable?: boolean;
-  /** 格式化列内容 */
-  formatter?: (row: any, column: any, cellValue: any, index: number) => string
-  /** 当内容过长被隐藏时显示 tooltip */
-  showOverflowTooltip?: boolean;
-  /** 对齐方式 */
-  align?: 'left' | 'center' | 'right';
-  /** 表头对齐方式，若不设置该项，则使用表格的对齐方式 */
-  headerAlign?: 'left' | 'center' | 'right';
-  /** 数据过滤的选项 */
-  filters?: Array<{ text: string, value: string }>;
-  /** 数据过滤使用的方法 */
-  filterMethod?: (value: any, row: any. column) => boolean;
-  /** 仅对type=selection的列有效 */
-  selectable?: (row: any, index: number) => boolean
-}
-
 /** Table Component */
 export declare class ElTable extends ElementUIComponent {
   /** Table data */
