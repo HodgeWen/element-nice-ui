35338faca212ebd3127a4a77692b65c3d903f6d0
whj <emailwen@qq.com>
2021-11-01 15:46:55 +0800

    update

--- theme-chalk/gulpfile.js
+++ theme-chalk/gulpfile.js
@@ -1,21 +1,20 @@
-'use strict';
+'use strict'
 
-const { series, src, dest } = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const cssmin = require('gulp-cssmin');
+const { series, src, dest } = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const cssmin = require('gulp-cssmin')
 
 function compile() {
   return src(['./src/*.scss', './src/*.css'])
-    .pipe(
-      sass())
+    .pipe(sass.sync().on('error', sass.logError))
     .pipe(cssmin())
-    .pipe(dest('./lib'));
+    .pipe(dest('./lib'))
 }
 
 function copyfont() {
   return src('./src/fonts/**')
     .pipe(cssmin())
-    .pipe(dest('./lib/fonts'));
+    .pipe(dest('./lib/fonts'))
 }
 
-exports.build = series(compile, copyfont);
+exports.build = series(compile, copyfont)
