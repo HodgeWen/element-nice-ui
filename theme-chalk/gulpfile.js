66b350f2aa2f8640460d499a24c9083441bbcb32
wen <emailwen@qq.com>
2025-07-28 15:01:09 +0800

    feat: update

--- theme-chalk/gulpfile.js
+++ theme-chalk/gulpfile.js
@@ -2,19 +2,17 @@
 
 const { series, src, dest } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
-const cssmin = require('gulp-cssmin')
+const cleanCSS = require('gulp-clean-css')
 
 function compile() {
   return src(['./src/*.scss', './src/*.css'])
     .pipe(sass.sync().on('error', sass.logError))
-    .pipe(cssmin())
+    .pipe(cleanCSS())
     .pipe(dest('./lib'))
 }
 
 function copyfont() {
-  return src('./src/fonts/**')
-    .pipe(cssmin())
-    .pipe(dest('./lib/fonts'))
+  return src('./src/fonts/**').pipe(dest('./lib/fonts'))
 }
 
 exports.build = series(compile, copyfont)
